1b0b77aaf37f9828a33bee5e2888aeb378eff7ca
konstantinlaskulov <konstantin.laschkulov@yandex.com>
2024-12-31 20:19:21 +0300

    primitive cart moving besides of it state

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -1,16 +1,84 @@
 extends StaticBody2D
 
 @export var facing_left_right = true
+var can_be_moved = false
+var direction := Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if facing_left_right:
-		$left_right.visible = true
-		$top_down.visible = false
+		enable_left_right()
 	else:
-		$left_right.visible = false
-		$top_down.visible = true
+		enable_top_down()
 
+func enable_left_right():
+	$left_right.visible = true
+	$top_down.visible = false
+	$top_area.monitoring = false
+	$down_area.monitoring = false
+	$left_area.monitoring = true
+	$right_area.monitoring = true
+
+func enable_top_down():
+	$left_right.visible = false
+	$top_down.visible = true
+	$top_area.monitoring = true
+	$down_area.monitoring = true
+	$left_area.monitoring = false
+	$right_area.monitoring = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("action") and can_be_moved:
+		print('moving!')
+		move()
+
+func move():
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + direction * 50, 0.3)
+
+func _on_top_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		direction = Vector2(0, 1)
+		can_be_moved = true
+
+
+func _on_top_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = false
+		direction = Vector2.ZERO
+
+
+func _on_down_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = true
+		direction = Vector2(0, -1)
+
+
+func _on_down_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = false
+		direction = Vector2.ZERO
+
+
+func _on_left_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = true
+		direction = Vector2(1, 0)
+
+
+func _on_left_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = false
+		direction = Vector2.ZERO
+
+
+func _on_right_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = true
+		direction = Vector2(-1, 0)
+
+
+func _on_right_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		can_be_moved = false
+		direction = Vector2.ZERO
