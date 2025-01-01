c9d868f813053be668a896295506097a458c07fa
konstantinlaskulov <konstantin.laschkulov@yandex.com>
2025-01-01 18:04:37 +0300

    level change system

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -4,11 +4,13 @@ extends Node2D
 @export var level_carts = 2
 @export var start_timeline: String = "None"
 @export var end_timeline: String = "None"
+@export var next_level : String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Globals.level_carts = level_carts
 	Globals.level_steps = level_steps
 	$ui.update_steps()
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	if start_timeline != "None":
 		$DialogueManager.play_diaologue(start_timeline)
 
@@ -28,6 +30,7 @@ func update_carts():
 	if Globals.level_carts - 1 == 0:
 		Globals.level_carts -= 1
 		if end_timeline != "None":
+			$Player.set_physics_process(false)
 			$DialogueManager.play_diaologue(end_timeline)
 	else:
 		Globals.level_carts -= 1
@@ -35,4 +38,6 @@ func update_carts():
 
 func _on_dialogic_signal(argument: String):
 	if argument == "end":
-		$Player.set_physics_process(true)
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_file(next_level)
