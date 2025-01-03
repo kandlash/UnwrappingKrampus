cf9596805a51d46bc217f5bf84bbe9e5aae54671
konstantinlaskulov <konstantin.laschkulov@yandex.com>
2025-01-03 15:13:55 +0300

    levels cosmetic changes

--- scripts/death_area.gd
+++ scripts/death_area.gd
@@ -8,4 +8,20 @@ func _on_area_entered(area: Area2D) -> void:
 		 "scale",
 		 Vector2.ZERO,
 		 0.3).set_trans(Tween.TRANS_SPRING)
-		$"..".reload_level()
+		$"../restart_delay".start()
+	if area.name == "cart_area":
+		var cart = area.get_parent()
+		if not cart.can_be_moved:
+			return
+		var tween = create_tween()
+		tween.tween_property(
+			cart,
+			"scale",
+			Vector2.ZERO,
+			0.3
+		).set_trans(Tween.TRANS_SPRING)
+		$"../restart_delay".start()
+
+
+func _on_restart_delay_timeout() -> void:
+	$"..".reload_level()
